9ad5ad7ac95c0f48d2d7c9993a1b156b19ff90c7
finnsnape <snapefinn@gmail.com>
2023-01-28 20:06:27 +0000

    Basic SvelteKit UI created
    Simple interface to prepare for setting options such as mutation rate

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<style lang="scss">
+    .app {
+        width: 1000px;
+        height: 800px;
+        border: 1px solid black;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        margin-top: $size-010;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        margin-top: $size-040;
+
+        input {
+            width: 300px;
+        }
+    }
+</style>
+
+<div class="app">
+    <h1 class="heading-2">Config</h1>
+
+    <form class="stack">
+        <label for="target">Target string</label>
+        <input type="text" name="target" value="The quick brown fox jumps over the lazy dog">
+
+        <label for="mutation-rate">Mutation rate (%)</label>
+        <input type="number" name="mutation-rate" min=0 max=100 value=5>
+
+        <label for="population-size">Population size</label>
+        <input type="number" name="population-size" min=0 max=10000 value=100>
+
+         <label for="max-generations">Max generations</label>
+        <input type="number" name="max-generations" min=0 value=0>
+
+        <label for="selection-mode">Selection mode</label>
+        <select name="selection-mode">
+            <option value="0">Roulette</option>
+            <option value="1">Tournament</option>
+        </select>
+
+        <label for="mating-mode">Mating mode</label>
+        <select name="mating-mode">
+            <option value="0">Single-point crossover</option>
+            <option value="1">Two-point crossover</option>
+        </select>
+
+        <button>Save</button>
+        <button>Run</button>
+    </form>
+</div>
