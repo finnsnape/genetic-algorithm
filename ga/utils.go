0cfe36b38a495760af457beae18d0ba0c0967a5b
finnsnape <snapefinn@gmail.com>
2023-03-08 21:34:59 +0000

    Added methods for random float in range, plus concatenating two slices

--- ga/utils.go
+++ ga/utils.go
@@ -1,14 +1,27 @@
 package main
 
-import(
-   "math/rand"
+import (
+	"math/rand"
 )
 
 func randStringRunes(n int) []byte {
-    var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.")
-    b := make([]rune, n)
-    for i := range b {
-        b[i] = letterRunes[rand.Intn(len(letterRunes))]
-    }
-    return []byte(string(b))
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ. ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return []byte(string(b))
+}
+
+func randFloat(min float64, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+// Safely concatenates two slices
+func concatSlices[A any](slice1 []A, slice2 []A) []A {
+	length := len(slice1) + len(slice2)
+	slice := make([]A, 0, length)
+	slice = append(slice, slice1...)
+	slice = append(slice, slice2...)
+	return slice
 }
