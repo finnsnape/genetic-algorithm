07c287cf93dba90fd99be3baeac16a1f23ec6a00
finnsnape <snapefinn@gmail.com>
2023-03-08 21:32:12 +0000

    Formatting

--- ga/evaluator.go
+++ ga/evaluator.go
@@ -1,17 +1,17 @@
 package main
 
 type Evaluator interface {
-    Evaluate(individual Individual, target []byte) float64
+	Evaluate(individual Individual, target []byte) float64
 }
 
-type ByteMatch struct {}
+type ByteMatch struct{}
 
 func (ByteMatch) Evaluate(individual Individual, target []byte) float64 {
-    fitness := 0
-    for i, b := range target {
-        if b == individual.genome[i] {
-            fitness++
-        }
-    }
-    return float64(fitness)/float64(len(target))
+	fitness := 0
+	for i, b := range target {
+		if b == individual.genome[i] {
+			fitness++
+		}
+	}
+	return float64(fitness) / float64(len(target))
 }
