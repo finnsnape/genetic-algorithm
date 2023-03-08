81179c207ee612426e6083c2801a75d6c6321b0f
finnsnape <snapefinn@gmail.com>
2023-03-08 16:38:29 +0000

    Moved Population struct to own file, and added a constructor

---
+++ ga/population.go
@@ -0,0 +1,23 @@
+package main
+
+type Population struct {
+    size uint
+    individuals []*Individual
+    totalFitness float64
+    fittestIndividual *Individual // maybe just keep sorted instead?
+    leastFitIndividual *Individual
+}
+
+func newPopulation(size uint, target []byte) *Population {
+    var individuals []*Individual
+    for i:=uint(0); i<size; i++ {
+        individual := newIndividual(len(target))
+        individuals = append(individuals, individual)
+    }
+    return &Population{
+        size: size,
+        individuals: individuals,
+        fittestIndividual: &Individual{fitness: 0.0},
+        leastFitIndividual: &Individual{fitness: 1.0},
+    }
+}
