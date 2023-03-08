f82f5d69f637386a43448672c126f8996690f96e
finnsnape <snapefinn@gmail.com>
2023-03-08 16:53:11 +0000

    Population waitgroup plus formatting
    Waitgroup added for later use

--- ga/population.go
+++ ga/population.go
@@ -1,23 +1,28 @@
 package main
 
+import (
+	"sync"
+)
+
 type Population struct {
-    size uint
-    individuals []*Individual
-    totalFitness float64
-    fittestIndividual *Individual // maybe just keep sorted instead?
-    leastFitIndividual *Individual
+	size               uint
+	individuals        []*Individual
+	totalFitness       float64
+	fittestIndividual  *Individual // maybe just keep sorted instead?
+	leastFitIndividual *Individual
+	waitGroup          *sync.WaitGroup
 }
 
 func newPopulation(size uint, target []byte) *Population {
-    var individuals []*Individual
-    for i:=uint(0); i<size; i++ {
-        individual := newIndividual(len(target))
-        individuals = append(individuals, individual)
-    }
-    return &Population{
-        size: size,
-        individuals: individuals,
-        fittestIndividual: &Individual{fitness: 0.0},
-        leastFitIndividual: &Individual{fitness: 1.0},
-    }
+	var individuals []*Individual
+	for i := uint(0); i < size; i++ {
+		individual := newIndividual(len(target))
+		individuals = append(individuals, individual)
+	}
+	return &Population{
+		size:               size,
+		individuals:        individuals,
+		fittestIndividual:  &Individual{fitness: 0.0},
+		leastFitIndividual: &Individual{fitness: 1.0},
+	}
 }
