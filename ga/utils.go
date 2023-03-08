490ba9b222102b3be6ef103c30e272b918b9b8ee
finnsnape <snapefinn@gmail.com>
2023-03-08 23:35:49 +0000

    Added utils for getting and printing image files

--- ga/utils.go
+++ ga/utils.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	_ "image/png"
 	"math/rand"
+	"os"
 )
 
 func randStringRunes(n int) []byte {
-	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ. ")
+	var letterRunes = []rune("01")
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
@@ -24,4 +28,27 @@ func concatSlices[A any](slice1 []A, slice2 []A) []A {
 	slice = append(slice, slice1...)
 	slice = append(slice, slice2...)
 	return slice
+}
+
+func getImageFromFilePath(filePath string) (image.Image, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	return img, err
+}
+
+func printImage(genome []byte, size int) {
+	for i := 0; i < size*size; i++ {
+		char := "⬜"
+		if genome[i] == []byte("1")[0] {
+			char = "⬛"
+		}
+		fmt.Print(char)
+		if (i+1)%size == 0 {
+			fmt.Print("\n")
+		}
+	}
 }
