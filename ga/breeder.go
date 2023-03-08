11d4d2cbde63e84e22035b8f914a18ae1e4cba0a
finnsnape <snapefinn@gmail.com>
2023-03-08 22:32:51 +0000

    Added uniform crossover, and placeholder for multi point crossover

--- ga/breeder.go
+++ ga/breeder.go
@@ -21,3 +21,35 @@ func (breeder OnePointCrossover) Breed(parent1 Individual, parent2 Individual) (
 	child2Genome := concatSlices(parent2.genome[0:randomNumber], parent1.genome[randomNumber:len(parent1.genome)])
 	return child1Genome, child2Genome
 }
+
+type MultiPointCrossover struct {
+	crossoverRate float64
+	n             int
+}
+
+func (breeder MultiPointCrossover) Breed(parent1 Individual, parent2 Individual) ([]byte, []byte) {
+	// FIXME
+	return parent1.genome, parent2.genome
+}
+
+type UniformCrossover struct {
+	crossoverRate float64
+}
+
+func (breeder UniformCrossover) Breed(parent1 Individual, parent2 Individual) ([]byte, []byte) {
+	if rand.Float64() > breeder.crossoverRate {
+		return parent1.genome, parent2.genome
+	}
+	child1Genome := make([]byte, 0, len(parent1.genome))
+	child2Genome := make([]byte, 0, len(parent1.genome))
+	for i := range parent1.genome {
+		if rand.Float64() > 0.5 {
+			child1Genome = append(child1Genome, parent1.genome[i])
+			child2Genome = append(child2Genome, parent2.genome[i])
+		} else {
+			child1Genome = append(child1Genome, parent2.genome[i])
+			child2Genome = append(child2Genome, parent1.genome[i])
+		}
+	}
+	return child1Genome, child2Genome
+}
