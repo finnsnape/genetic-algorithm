dc733c6a6ff9315420aef4efd4c0bd2f954841bd
finnsnape <snapefinn@gmail.com>
2023-03-08 21:35:50 +0000

    Boolean for solution, new breeding, cleaner print

--- ga/genetic_algorithm.go
+++ ga/genetic_algorithm.go
@@ -1,18 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type GeneticAlgorithm struct {
 	selector       Selector
 	evaluator      Evaluator
 	breeder        Breeder
 	mutator        Mutator
-	population     *Population
+	population     Population
 	maxGenerations uint
 	target         []byte
 	generation     uint
+	solved         bool
 }
 
 func newGeneticAlgorithm(selector Selector, evaluator Evaluator, breeder Breeder, mutator Mutator, maxGenerations uint, target []byte, populationSize uint) *GeneticAlgorithm {
@@ -25,50 +24,43 @@ func newGeneticAlgorithm(selector Selector, evaluator Evaluator, breeder Breeder
 		maxGenerations: maxGenerations,
 		target:         target,
 		generation:     0,
+		solved:         false,
 	}
 }
 
 func (ga *GeneticAlgorithm) evaluate() {
 	ga.population.totalFitness = 0.0
-	for i := uint(0); i < ga.population.size; i++ {
-		individual := ga.population.individuals[i]
-		individual.fitness = ga.evaluator.Evaluate(*individual, ga.target)
-		ga.population.totalFitness += individual.fitness
-		if individual.fitness > ga.population.fittestIndividual.fitness {
-			fmt.Println("fit", individual.fitness)
-			ga.population.fittestIndividual = individual
-		} else if individual.fitness < ga.population.leastFitIndividual.fitness {
-			fmt.Println("unfit", individual.fitness)
-			ga.population.leastFitIndividual = individual
+	for i, individual := range ga.population.individuals {
+		fitness := ga.evaluator.Evaluate(individual, ga.target)
+		ga.population.individuals[i].fitness = fitness
+		ga.population.totalFitness += fitness
+		if fitness == 1.0 {
+			ga.solved = true
 		}
 	}
 }
 
 func (ga *GeneticAlgorithm) breed() {
-	var individuals []*Individual
+	var individuals []Individual
 	for uint(len(individuals)) < ga.population.size {
-		parent1 := ga.selector.Select(*ga.population)
-		parent2 := ga.selector.Select(*ga.population)
-		child := &Individual{
-			genome: ga.breeder.Breed(parent1, parent2),
-		}
-		child.fitness = ga.evaluator.Evaluate(*child, ga.target)
-		individuals = append(individuals, child)
+		parent1 := ga.selector.Select(ga.population)
+		parent2 := ga.selector.Select(ga.population)
+		child1Genome, child2Genome := ga.breeder.Breed(parent1, parent2)
+		child1, child2 := Individual{genome: child1Genome, fitness: 0.0}, Individual{genome: child2Genome, fitness: 0.0}
+		individuals = append(individuals, child1)
+		individuals = append(individuals, child2)
 	}
 	ga.population.individuals = individuals
 }
 
 func (ga *GeneticAlgorithm) mutate() {
-	for _, individual := range ga.population.individuals {
-		individual.genome = ga.mutator.Mutate(*individual)
+	for i, individual := range ga.population.individuals {
+		ga.population.individuals[i].genome = ga.mutator.Mutate(individual)
 	}
 }
 
 func (ga *GeneticAlgorithm) print() {
-	fmt.Println("Generation: ", ga.generation)
-	fmt.Println("Average fitness: ", ga.population.totalFitness/float64(ga.population.size))
-	fmt.Println("Best fitness: ", ga.population.fittestIndividual.fitness)
-	fmt.Println("Best solution: ", string(ga.population.fittestIndividual.genome))
+	fmt.Printf("[%d] %.3f", ga.generation, ga.population.totalFitness/float64(ga.population.size))
 	fmt.Println()
 }
 
@@ -83,9 +75,11 @@ func (ga *GeneticAlgorithm) evolve() {
 func (ga *GeneticAlgorithm) simulate() {
 	for {
 		ga.evolve()
-		if ga.population.fittestIndividual.fitness == 1.0 {
+		if ga.solved {
+			fmt.Println("Solved")
 			break // solution found
 		} else if ga.generation > ga.maxGenerations && ga.maxGenerations != 0 {
+			fmt.Println("Maximum generations reached")
 			break // exceeded max generations
 		}
 	}
