8df122d05e07bfc373f84beff7cca53e2b134230
finnsnape <snapefinn@gmail.com>
2023-03-08 21:34:27 +0000

    Implemented Roulette selection

--- ga/selector.go
+++ ga/selector.go
@@ -1,43 +1,48 @@
 package main
 
 import (
-    "math/rand"
+	"math/rand"
 )
 
 type Selector interface {
-    Select(population Population) Individual
+	Select(population Population) Individual
 }
 
 type Tournament struct {
-    tournamentSize uint
-}
-func (selector Tournament) Select(population Population) Individual {
-    var competitors []Individual
-    for i:=uint(0); i<selector.tournamentSize; i++ {
-        randomNumber := rand.Intn(int(population.size))
-        competitor := population.individuals[randomNumber]
-        competitors = append(competitors, *competitor)
-    }
-    winner := competitors[0]
-    for _, competitor := range competitors {
-        if competitor.fitness <= winner.fitness {
-            continue
-        }
-        winner = competitor
-    }
-    return winner
+	tournamentSize uint
 }
 
-type Roulette struct {}
+func (selector Tournament) Select(population Population) Individual {
+	var competitors []Individual
+	for i := uint(0); i < selector.tournamentSize; i++ {
+		randomNumber := rand.Intn(int(population.size)) // range?
+		competitor := population.individuals[randomNumber]
+		competitors = append(competitors, competitor)
+	}
+	winner := competitors[0]
+	for _, competitor := range competitors {
+		if competitor.fitness < winner.fitness {
+			continue
+		}
+		winner = competitor
+	}
+	return winner
+}
+
+type Roulette struct{}
 
 func (Roulette) Select(population Population) Individual {
-    randomNumber := rand.Float64()
-    probSum := 0.0
-    for _, individual := range population.individuals {
-        probSum += individual.fitness/population.totalFitness
-        if randomNumber < probSum {
-            return *individual
-        }
-    }
-    panic("Couldn't select a fitness")
+	//if population.totalFitness == 0 {
+	//	randomNumber := rand.Intn(len(population.individuals))
+	//	return population.individuals[randomNumber]
+	//}
+	randomNumber := rand.Float64()
+	probabilitySum := 0.0
+	for _, individual := range population.individuals {
+		probabilitySum += individual.fitness / population.totalFitness
+		if randomNumber < probabilitySum {
+			return individual
+		}
+	}
+	panic("Couldn't select a fitness")
 }
