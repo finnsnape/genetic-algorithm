b0558b4ab61bb2a9fa3e1c3d6ffb02aef0103c6d
finnsnape <snapefinn@gmail.com>
2023-03-08 21:31:35 +0000

    Crossover now returns two children and uses probability

--- ga/breeder.go
+++ ga/breeder.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-    "math/rand"
+	"math/rand"
 )
 
 type Breeder interface {
-    Breed(parent1 Individual, parent2 Individual) []byte
+	Breed(parent1 Individual, parent2 Individual) ([]byte, []byte)
 }
 
-type OnePointCrossover struct {}
+type OnePointCrossover struct {
+	crossoverRate float64
+}
 
-func (OnePointCrossover) Breed(parent1 Individual, parent2 Individual) []byte {
-    randomNumber := rand.Intn(len(parent1.genome))
-    childGenome := append(parent1.genome[0:randomNumber], parent2.genome[randomNumber:len(parent1.genome)]...)
-    return childGenome
+func (breeder OnePointCrossover) Breed(parent1 Individual, parent2 Individual) ([]byte, []byte) {
+	if rand.Float64() > breeder.crossoverRate {
+		return parent1.genome, parent2.genome
+	}
+	randomNumber := rand.Intn(len(parent1.genome))
+	child1Genome := concatSlices(parent1.genome[0:randomNumber], parent2.genome[randomNumber:len(parent1.genome)])
+	child2Genome := concatSlices(parent2.genome[0:randomNumber], parent1.genome[randomNumber:len(parent1.genome)])
+	return child1Genome, child2Genome
 }
