32e99c6c56455c6ed204727aaaa650ca77d67710
finnsnape <snapefinn@gmail.com>
2023-04-19 17:26:08 +0100

    Made navigation header

---
+++ ui/src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<style lang="scss">
+    nav {
+      display: flex;
+      justify-content: space-evenly;
+      padding: $size-010;
+      background-color: lightgray;
+    }
+</style>
+
+<nav>
+    <a href="/learn">Learn</a>
+    <a href="/create">Create</a>
+    <a href="/run">Run</a>
+</nav>
