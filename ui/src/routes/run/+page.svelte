5a6b3283a43c3ffd6ffd29c95f155dee7412167c
finnsnape <snapefinn@gmail.com>
2023-04-19 17:28:37 +0100

    Made basic run page to select functions and run GA

---
+++ ui/src/routes/run/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    /** @type {import('./$types').PageData} */  export let data;
+</script>
+
+<style lang="scss">
+    .result {
+      margin: $size-030 auto;
+      width: 500px;
+      height: 500px;
+      border: 1px solid black;
+    }
+</style>
+
+<form method="POST" class="stack">
+    {#each Object.entries(data) as [name, func]}
+        <div>
+            <label>{func.title}</label>
+            <select>
+                <option>N/A></option>
+            </select>
+        </div>
+    {/each}
+    <button>Run</button>
+</form>
+
+<div class="result">
+
+</div>
