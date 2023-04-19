4eed8d8dc03147b63b8237ee882021e16eed15f6
finnsnape <snapefinn@gmail.com>
2023-04-19 17:24:36 +0100

    Created TS type for creating a custom function

--- ui/src/lib/ts/types.ts
+++ ui/src/lib/ts/types.ts
@@ -1,4 +1,13 @@
-export type Image = {
-  alt: string,
-  src: string,
+export type Function = {
+  title: string,
+  description: string,
+  name: string,
+  placeholder: string,
+  inputs: Variable[],
+  outputs: Variable[]
+}
+
+type Variable = {
+  name: string,
+  type: string,
 }
