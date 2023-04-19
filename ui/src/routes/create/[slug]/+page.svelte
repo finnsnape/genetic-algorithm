05b65d1d7984a30d6115ab1333f10527bcd5ddeb
finnsnape <snapefinn@gmail.com>
2023-04-19 17:28:18 +0100

    Made use of Create component

---
+++ ui/src/routes/create/[slug]/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import Create from "$lib/components/Create.svelte";
+    /** @type {import('./$types').PageData} */  export let data;
+</script>
+
+<Create func={data}></Create>
